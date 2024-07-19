d2d8b0312778e6dd8bc77fde268af3d57e15e212
Maria Eduarda <mariaeduardafbraz@outlook.com>
2024-07-19 13:29:17 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.Conexao;
 import model.dao.impl.SellerDaoJDBC;
 
 //Vai ser responsável por instanciar os daos
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() { //dessa forma não precisamos expor a implementação do sellerDao
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(Conexao.getConnection());
 	}
 }
